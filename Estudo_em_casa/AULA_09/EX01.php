b065dc2b167794d57ea78051e6262d3d175030e6
Larissa Souza <larissa.iranilda@hotmail.com>
2024-09-28 14:11:33 -0300

    Operadores Aritméticos do PHP

---
+++ Estudo_em_casa/AULA_09/EX01.php
@@ -0,0 +1,7 @@
+<?php 
+//Operador de concatenação . (junção)
+    $r = "3" . "3";
+    echo "$r <br>";
+
+    var_dump($r);
+?>
---
+++ Estudo_em_casa/AULA_09/EX02.php
@@ -0,0 +1,45 @@
+<?php 
+
+// Operadores Aritméticos do PHP
+// Operadores normais 
+// +  Adição
+// - Subtração
+// * Multiplicação
+//  / Divisão Real
+//  % Módulo (Ele calcula o resto da divisão inteira )
+// ** Potência
+
+    $r = 5 + 2;
+
+    echo "Resulta de 5 por algum operador 2 <br>";
+
+   var_dump($r);
+   echo "Multiplicação <br>";
+
+   $r = 5 - 2;
+
+   var_dump($r);
+   echo "Subtração <br>";
+
+   $r = 5 * 2;
+
+   var_dump($r);
+   echo "Multiplicação <br>";
+
+   $r = 5 / 2;
+
+   var_dump($r);
+   echo "Divisão Real <br>";
+
+   $r = 5 % 2;
+
+   var_dump($r);
+   echo "Módulo <br>";
+
+   $r = 5 ** 2;
+
+   var_dump($r);
+   echo "Potência <br>";
+
+
+?>
